b355f058816d8500baaf55049d71d63cf66c107a
phuchoa <phuchoa1202@gmail.com>
2024-02-06 15:46:18 +0700

    Hoa/viet_tu_dong_build_15phut_cho_vps

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Lunar-New-Year-Mini-Games-Project',
+      exec_mode: 'cluster',
+      instances: 1, // Chỉ chạy một instance
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start'
+    }
+  ]
+}
