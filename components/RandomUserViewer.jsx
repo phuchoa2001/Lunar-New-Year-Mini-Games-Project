d7751ee025c5ce00e0fb2400458f2f0e1132b50c
phuchoa <phuchoa1202@gmail.com>
2024-02-13 12:59:32 +0700

    Hoa/them_loading_cho_nut_ngau_nhien

--- components/RandomUserViewer.jsx
+++ components/RandomUserViewer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Toast } from 'antd-mobile';
 import { useRandomGoal } from 'hooks/swr/useGoal';
 import useAuth from 'hooks/useAuth';
@@ -7,13 +7,16 @@ import { useRouter } from 'next/router';
 
 function RandomUserViewer(props) {
   const { user, isLoading } = useAuth();
+  const [isLoadingApi, setIsLoadingApi] = useState(false);
   const router = useRouter();
   const randomGoal = useRandomGoal();
 
   const handleRandom = async () => {
+    setIsLoadingApi(true)
     const res = await randomGoal(user.idUser)
     if (res.isSuccess) {
       router.push(`/goals/detail/${res.data['_id']}?title=${res.data.target}`)
+      setIsLoadingApi(false);
     } else {
       Toast.show({
         content: 'Không có dữ liệu',
@@ -23,7 +26,7 @@ function RandomUserViewer(props) {
 
   return (
     <Button block color='primary' size='middle' onClick={handleRandom} className='ant-button'>
-      {isLoading ? <LoadingComponent /> : "Xem người ngẫu nhiên"}
+      {(isLoading || isLoadingApi) ? <LoadingComponent /> : "Xem người ngẫu nhiên"}
     </Button>
   );
 }
