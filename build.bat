ca05c660f79d38fe9a0a76f34ab809bb7d77c5a6
Đặng Phúc Hòa <95266292+phuchoa2001@users.noreply.github.com>
2024-02-17 13:31:40 +0700

    Hoa/them_tinh_nang_neu_khong_action_thi_build (#42)

--- build.bat
+++ build.bat
@@ -1,6 +1,16 @@
 @echo off
 
-echo Building the project...
-npm run build || echo Build failed, continuing with the script...
+echo Checking API condition...
+curl -s "https://lunar-new-year-mini-games-project-server.vercel.app/checkActions" > api_response.txt
 
-echo All processes have been successfully started.
+set /p API_CONDITION=<api_response.txt
+
+if "%API_CONDITION%"=="true" (
+    echo Building the project...
+    npm run build || echo Build failed, continuing with the script...
+    echo All processes have been successfully started.
+) else (
+    echo Condition not met, skipping build.
+)
+
+del api_response.txt
--- pm2reload.bat
+++ pm2reload.bat
@@ -1,4 +1,19 @@
 @echo off
-echo Reloading the application with PM2...
-pm2 reload all
-echo All processes have been successfully started.
+
+echo Checking API condition...
+curl -s "https://lunar-new-year-mini-games-project-server.vercel.app/checkActions" > api_response.txt
+
+set /p API_CONDITION=<api_response.txt
+
+if "%API_CONDITION%"=="true" (
+    echo Building the project...
+    npm run build || echo Build failed, continuing with the script...
+    echo All processes have been successfully started.
+    echo Reloading the application with PM2...
+    pm2 reload all
+    echo Application has been successfully reloaded.
+) else (
+    echo Condition not met, skipping build and reload.
+)
+
+del api_response.txt
