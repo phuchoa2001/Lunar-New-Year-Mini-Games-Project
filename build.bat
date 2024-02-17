65054fb1243bdb820876dd5ab36159207576b517
phuchoa <phuchoa1202@gmail.com>
2024-02-17 13:59:50 +0700

    Hoa/them_tinh_nang_khong_co_action_thi_khong_build

--- build.bat
+++ build.bat
@@ -1,10 +1,5 @@
 @echo off
 
-echo Checking API condition...
-curl -s "https://lunar-new-year-mini-games-project-server.vercel.app/checkActions" > api_response.txt
-
-set /p API_CONDITION=<api_response.txt
-
 if "%API_CONDITION%"=="true" (
     echo Building the project...
     npm run build || echo Build failed, continuing with the script...
@@ -12,5 +7,3 @@ if "%API_CONDITION%"=="true" (
 ) else (
     echo Condition not met, skipping build.
 )
-
-del api_response.txt
