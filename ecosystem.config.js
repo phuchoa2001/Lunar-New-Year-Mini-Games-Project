4a6ca48c3c7bd9b4ec784f10575e13e05033ba7d
phuchoa <phuchoa1202@gmail.com>
2024-02-17 14:28:48 +0700

    Revert "Hoa/mo_lai_port_80- (#41)"
    This reverts commit ade4b959cda4b9ef0acaf1e27949f338d719f222.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 1, // Chỉ chạy một instance
       script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 80'
+      args: 'start -p 443'
     }
   ]
 }
